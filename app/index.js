7f1f2aa4b924841a7e06802b1f4349dfb26babdd
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2024-03-06 10:50:42 -0500

    Añadir registros de apertura y cierre de túnel

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 const localtunnel = require('localtunnel');
 
 (async () => {
+  console.log('Opening tunnel');
+
   const tunnel = await localtunnel({ port: 3000 });
 
+  console.log('Tunnel opened');
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
   tunnel.url;
